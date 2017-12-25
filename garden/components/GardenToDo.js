6ece1a6bdfcef026ea8052be150b69ae74c22be4
MeghanHughes <meghan@manu.net.nz>
2017-12-25 20:23:51 +1300

    finally figured out how to style components

--- garden/components/GardenToDo.js
+++ garden/components/GardenToDo.js
@@ -1,9 +1,26 @@
 import h from 'react-hyperscript'
+import { FormattedMessage } from 'dogstack/intl'
+import { compose } from 'recompose'
+import { connect as connectStyles } from 'react-fela'
+
+import styles from '../styles/GardenToDo'
 
 const GardenToDo = (props) => {
+  const { styles } = props
+
   return (
-    h('p', {}, 'Garden To Do List')
+    h('div', {
+      className: styles.container
+    }, [
+      h('h1', {}, [
+        h(FormattedMessage, {
+          id: 'garden.toDoList'
+        })
+      ])
+    ])
   )
 }
 
-export default GardenToDo
+export default compose(
+  connectStyles(styles)
+)(GardenToDo)
